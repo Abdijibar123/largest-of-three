6bc5b920b30021d447bde1edac2dc2652773a57c
Abdijibar123 <abdijibar1313@gmail.com>
2023-05-29 16:40:34 +0300

    done with three numbers

--- challenge.js
+++ challenge.js
@@ -1,2 +1,14 @@
-// Good Luck! You got this 💪🏾
-// Write your code here.
+const var1 = 20
+const var2 = 15
+const var3 = 10
+if(var1 > var2 && var1 > var3){
+    console.log(`the largest number is ${var1}`)
+}
+
+else if(var2 > var1 && var2 > var3){
+    console.log(`the largest number is ${var2}`)
+}
+
+else {
+    console.log(`the largest number is ${var3}`)
+}
